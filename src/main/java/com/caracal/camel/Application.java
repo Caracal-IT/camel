70dfb52690be27ef8895469a7f1991028be489d3
EttieneMare <ettiene/mare@divigraph.com>
2023-04-03 11:42:44 +0200

    Add Mqtt

--- src/main/java/com/caracal/camel/Application.java
+++ src/main/java/com/caracal/camel/Application.java
@@ -2,10 +2,20 @@ package com.caracal.camel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
+@Import({
+		com.caracal.camel.web.controllers.HomeController.class})
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
---
+++ src/main/java/com/caracal/camel/web/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.caracal.camel.web.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+// https://open-api.augury.com/api-docs
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("appName", appName);
+
+        return "home";
+    }
+}
