e20267e292018239e256d9e95e1615cf5a2dab22
EttieneMare <ettiene/mare@divigraph.com>
2023-05-03 08:13:22 +0200

    Add Security

--- certs/site/certs_register_host.ps1
+++ certs/site/certs_register_host.ps1
@@ -1,3 +1,4 @@
 ﻿Install-Module -Name 'Carbon' -AllowClobber
 Import-Module 'Carbon'
 Set-CHostsEntry -IPAddress 127.0.0.1 -HostName 'dev.caracal.com' -Description "Caracal Dev Environment"
+Set-CHostsEntry -IPAddress 127.0.0.1 -HostName 'camel_db' -Description "Caracal Dev Environment - Database"
