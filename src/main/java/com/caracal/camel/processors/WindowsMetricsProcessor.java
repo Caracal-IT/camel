4da8d85835e88c1dcf3cde36bd188cc594ad3df1
ettienemare <ettienemare@gmail.com>
2023-05-05 19:19:39 +0200

    Add Postgres Support.

--- src/main/java/com/caracal/camel/processors/WindowsMetricsProcessor.java
+++ src/main/java/com/caracal/camel/processors/WindowsMetricsProcessor.java
@@ -1,6 +1,7 @@
 package com.caracal.camel.processors;
 
 import com.caracal.camel.routes.models.WindowsServiceMetric;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
@@ -22,45 +23,52 @@ public class WindowsMetricsProcessor implements Processor {
         var mapper = new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        for (Object messageValues : messages) {
-            @SuppressWarnings("unchecked")
-            var message = (Map<String, String>) messageValues;
-            var cat = message.get("cat");
-
-            if(!Objects.equals(cat, "info") && !Objects.equals(cat, "state"))
-                continue;
-
-            var flag = message.get("flag");
-
-            var body = message.get("body")
-                    .replace("=", ":")
-                    .replaceAll("(\\w+):", "\"$1\":")
-                    .trim();
-
-            if(!body.startsWith("{") || !body.endsWith("}"))
-                continue;
-
-            var metric = mapper.readValue(body, WindowsServiceMetric.class);
-            var displayName = metric.getDisplayName();
-            var state = metric.getState();
-
-            if(!services.containsKey(metric.getName())) {
-                services.put(metric.getName(), metric);
-                metric.setAppNamespace(appNamespace);
-                metric.setUtcDateString(Instant.now().toString());
-            }
-            else
-                metric = services.get(metric.getName());
-
-            if(Objects.equals(cat, "info"))
-                metric.setDisplayName(displayName);
-
-            if(Objects.equals(cat, "state") && Objects.equals(flag, "1")) {
-                metric.setCount(1);
-                metric.setState(state);
-            }
-        }
+        for (Object messageValues : messages)
+            addServiceMetrics(services, mapper, (Map<String, String>) messageValues);
 
         exchange.getIn().setBody(services.values());
+    }
+
+    private void addServiceMetrics(HashMap<String, WindowsServiceMetric> services, ObjectMapper mapper, Map<String, String> messageValues) throws JsonProcessingException {
+        var cat = messageValues.get("cat");
+
+        if(!Objects.equals(cat, "info") && !Objects.equals(cat, "state"))
+            return;
+
+        String body = convertToJson(messageValues);
+
+        if(!body.startsWith("{") || !body.endsWith("}"))
+            return;
+
+        var flag = messageValues.get("flag");
+        var metric = mapper.readValue(body, WindowsServiceMetric.class);
+        var displayName = metric.getDisplayName();
+        var state = metric.getState();
+
+        if(!services.containsKey(metric.getName()))
+            createServiceMetric(services, metric);
+        else
+            metric = services.get(metric.getName());
+
+        if(Objects.equals(cat, "info"))
+            metric.setDisplayName(displayName);
+
+        if(Objects.equals(cat, "state") && Objects.equals(flag, "1")) {
+            metric.setCount(1);
+            metric.setState(state);
+        }
+    }
+
+    private static String convertToJson(Map<String, String> messageValues) {
+        return messageValues.get("body")
+                            .replace("=", ":")
+                            .replaceAll("(\\w+):", "\"$1\":")
+                            .trim();
+    }
+
+    private void createServiceMetric(HashMap<String, WindowsServiceMetric> services, WindowsServiceMetric metric) {
+        services.put(metric.getName(), metric);
+        metric.setAppNamespace(appNamespace);
+        metric.setUtcDateString(Instant.now().toString());
     }
 }
